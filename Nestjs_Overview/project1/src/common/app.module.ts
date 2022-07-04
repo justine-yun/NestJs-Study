a93a54eaca68e34cff792eedb7d011a4f6ffb0af
beepbipbp <riskyjunk@gmail.com>
2022-07-04 23:04:48 +0900

    Chapter 5-2

--- Nestjs_Overview/project1/src/common/app.module.ts
+++ Nestjs_Overview/project1/src/common/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from '../cats/cats.module';
 import { LoggerMiddleware } from './middlewares/logger.middleware'
+import { MongooseModule } from "@nestjs/mongoose";
+import { ConfigModule } from '@nestjs/config'
 
 @Module({
-  imports: [CatsModule],
+  imports: [ConfigModule.forRoot(), MongooseModule.forRoot(process.env.MONGODB_URI), CatsModule],
   controllers: [AppController],
   providers: [AppService],
 })
