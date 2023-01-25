29589957954be7021fc8deebdf14608b1d024cc7
beepbipbp <riskyjunk@gmail.com>
2023-01-26 03:07:57 +0900

    project 초기화

--- Nestjs_Overview/project1/src/auth/dto/login.request.dto.ts
+++
@@ -1,4 +0,0 @@
-import {Cat} from "../../cats/cats.schema";
-import {PickType} from "@nestjs/swagger";
-
-export class LoginRequestDto extends PickType(Cat, ['email', 'password'] as const) {}
--- Nestjs_Overview/project1/src/cats/dto/cats.request.dto.ts
+++
@@ -1,4 +0,0 @@
-import {Cat} from "../cats.schema";
-import {PickType} from "@nestjs/swagger";
-
-export class CatRequestDto extends PickType(Cat, ['email', 'name', 'password'] as const) {}
