6433791ff8ec61ef4f4444f73c45a4409026bf3a
beepbipbp <riskyjunk@gmail.com>
2023-02-07 16:42:27 +0900

    mongodb 연결

--- project3/src/app.module.ts
+++ project3/src/app.module.ts
@@ -1,15 +1,23 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module, NestModule } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { MongooseModule } from "@nestjs/mongoose";
+import mongoose from "mongoose";
+import { AppController } from "./app.controller";
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  private readonly isDev = process.env.MODE === "dev";
+
+  configure() {
+    mongoose.set("debug", this.isDev);
+  }
+}
