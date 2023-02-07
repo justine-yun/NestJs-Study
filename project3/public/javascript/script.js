079553830315afecdaf95bdfad979149e447ad02
beepbipbp <riskyjunk@gmail.com>
2023-02-08 01:50:45 +0900

    socket 연결 확인

--- project3/public/javascript/script.js
+++ project3/public/javascript/script.js
@@ -8,6 +8,10 @@ const formElement = getElementById("chat_form");
 
 function helloUser() {
   const userName = prompt("What is your name?");
+
+  socket.emit("new_user", userName);
+
+  socket.on("hello_user", (data) => console.log(data));
 }
 
 function init() {
--- project3/src/chat/chat.gateway.ts
+++ project3/src/chat/chat.gateway.ts
@@ -1,9 +1,10 @@
-import { SubscribeMessage, WebSocketGateway } from "@nestjs/websockets";
+import { ConnectedSocket, MessageBody, SubscribeMessage, WebSocketGateway } from "@nestjs/websockets";
+import { Socket } from "socket.io";
 
 @WebSocketGateway()
 export class ChatGateway {
-  @SubscribeMessage("message")
-  handleMessage(client: any, payload: any): string {
-    return "Hello world!";
+  @SubscribeMessage("new_user")
+  handleMessage(@MessageBody() userName: string, @ConnectedSocket() socket: Socket) {
+    socket.emit("hello_user", "hello " + userName);
   }
 }
